c89e05823018cb662c0831ffdddfc0cb516760dd
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-29 17:37:39 +0530

    Update Destination city.cpp

--- DAY  9 - 1436 /Destination city.cpp
+++ DAY  9 - 1436 /Destination city.cpp
@@ -22,3 +22,28 @@ public:
         return "";
     }
 };
+
+//Approach-2 (Using HashSet) - Count Outdegrees
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> st;
+        
+        for(auto &path : paths) {
+            string source = path[0]; //source
+            st.insert(source);
+        }
+        
+        for(auto &path : paths) {
+            string dest = path[1];
+            
+            if(st.find(dest) == st.end()) {
+                return dest;
+            }
+        }
+        
+        return "";
+    }
+};
