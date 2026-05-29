fdf30cc7c3305089981129f6d57c180ef0145d24
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-29 17:40:05 +0530

    Create DestinationCity.cpp

---
+++ DAY 9 - 1436 /DestinationCity.cpp
@@ -0,0 +1,49 @@
+//Approach-1 (Using HashMap) - Count Outdegrees
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string, int> mp;
+        
+        for(auto &path : paths) {
+            string source = path[0]; //outdegree
+            mp[source]++;
+        }
+        
+        for(auto &path : paths) {
+            string dest = path[1];
+            
+            if(mp[dest] == 0) {
+                return dest;
+            }
+        }
+        
+        return "";
+    }
+};
+
+//Approach-2 (Using HashSet) - Count Outdegrees
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> st;
+        
+        for(auto &path : paths) {
+            string source = path[0]; //source
+            st.insert(source);
+        }
+        
+        for(auto &path : paths) {
+            string dest = path[1];
+            
+            if(st.find(dest) == st.end()) {
+                return dest;
+            }
+        }
+        
+        return "";
+    }
+};
