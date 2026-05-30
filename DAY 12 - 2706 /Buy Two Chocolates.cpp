fad33141296e2c3262df614bddcfdfec162cba52
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-30 15:15:45 +0530

    Update Buy Two Chocolates.cpp

--- DAY 12 - 2706 /Buy Two Chocolates.cpp
+++ DAY 12 - 2706 /Buy Two Chocolates.cpp
@@ -1,21 +1,51 @@
+//Approach (Simply find min and second min to check 2 chocolates price)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        int minPrice = INT_MAX;
-        int secminPrice = INT_MAX;
-
-        for(int &price : prices){
-            if(price<minPrice){
-                secminPrice = minPrice;
-                minPrice = price;
-            }
-            else{
-                                             secminPrice = min(secminPrice,price);
+        int minPrice    = INT_MAX;
+        int secMinPrice = INT_MAX;
+        
+        for(int &price : prices) {
+            if(price < minPrice) {
+                secMinPrice = minPrice;
+                minPrice    = price;
+            } else {
+                secMinPrice = min(secMinPrice, price);
             }
         }
-        if(minPrice + secminPrice > money){
-        return money;
-        }
-        return money - (minPrice + secminPrice);
+        
+        if(minPrice + secMinPrice > money)
+            return money;
+        
+        return money - (minPrice + secMinPrice);
     }
 };
+
+
+
+//Approach (Simply find min and second min to check 2 chocolates price)
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int minPrice = Integer.MAX_VALUE;
+        int secMinPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                secMinPrice = minPrice;
+                minPrice = price;
+            } else {
+                secMinPrice = Math.min(secMinPrice, price);
+            }
+        }
+
+        if (minPrice + secMinPrice > money) {
+            return money;
+        }
+
+        return money - (minPrice + secMinPrice);
+    }
+}
