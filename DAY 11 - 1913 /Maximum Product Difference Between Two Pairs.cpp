1c1eeefa353d8e23b4c320e821b13664015daf15
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-30 15:09:18 +0530

    Implement max product difference without sorting
    Added an alternative approach to calculate the maximum product difference without sorting.

--- DAY 11 - 1913 /Maximum Product Difference Between Two Pairs.cpp
+++ DAY 11 - 1913 /Maximum Product Difference Between Two Pairs.cpp
@@ -1,3 +1,6 @@
+//Approach-1 (Using sorting)
+//T.C : O(nlogn) 
+//S.C : O(1)
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
@@ -5,5 +8,40 @@ public:
         sort(begin(nums), end(nums));
         
         return (nums[n-1] * nums[n-2]) - (nums[1] * nums[0]);
+    }
+};
+
+
+//Approach-1 (Without sorting)
+//T.C : O(n) 
+//S.C : O(1)
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int largest = INT_MIN;
+        int secondLargest = INT_MIN;
+        
+        int smallest = INT_MAX;
+        int secondSmallest = INT_MAX;
+        
+        for(int &num : nums) {
+            if(num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else {
+                secondLargest = max(secondLargest, num);
+            }
+            
+            if(num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else {
+                secondSmallest = min(secondSmallest, num);
+            }
+            
+        }
+        
+        return (largest * secondLargest) - (secondSmallest * smallest);
+        
     }
 };
