09be013d95b5457e141d3cb779cadc11521bcafa
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-05-30 15:12:18 +0530

    Implement max product difference in Java and C++

--- DAY 11 - 1913 /Maximum Product Difference Between Two Pairs.cpp
+++ DAY 11 - 1913 /Maximum Product Difference Between Two Pairs.cpp
@@ -1,3 +1,4 @@
+/************************************************************ C++ ********************************************************************************/
 //Approach-1 (Using sorting)
 //T.C : O(nlogn) 
 //S.C : O(1)
@@ -45,3 +46,49 @@ public:
         
     }
 };
+
+
+/************************************************************ JAVA ********************************************************************************/
+//Approach-1 (Using sorting)
+//T.C : O(nlogn) 
+//S.C : O(1)
+public class Solution {
+    public int maxProductDifference(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        return (nums[n - 1] * nums[n - 2]) - (nums[1] * nums[0]);
+    }
+}
+
+
+//Approach-1 (Without sorting)
+//T.C : O(n) 
+//S.C : O(1)
+public class Solution {
+    public int maxProductDifference(int[] nums) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else {
+                secondLargest = Math.max(secondLargest, num);
+            }
+            
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else {
+                secondSmallest = Math.min(secondSmallest, num);
+            }
+        }
+        
+        return (largest * secondLargest) - (secondSmallest * smallest);
+    }
+}
