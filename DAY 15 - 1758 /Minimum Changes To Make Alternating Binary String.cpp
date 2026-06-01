bade05e4b951046f18c1e42c7cb46f4754178e9c
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-01 22:41:49 +0530

    Optimize minOperations for alternating binary string
    Refactor minOperations function to calculate minimum changes for alternating binary string with a single approach.

--- DAY 15 - 1758 /Minimum Changes To Make Alternating Binary String.cpp
+++ DAY 15 - 1758 /Minimum Changes To Make Alternating Binary String.cpp
@@ -29,3 +29,32 @@ public:
         return min(start_with_0, start_with_1);
     }
 };
+
+//Approach-2 (No need to calculate both)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.length();
+        int start_with_0 = 0;
+        
+        //010101
+        //101010
+        for(int i = 0; i<n; i++) {
+            if(i%2 == 0) {
+                if(s[i] == '1') {
+                    start_with_0++;
+                }
+            } else {
+                if(s[i] == '0') {
+                    start_with_0++;
+                }
+            }
+        }
+        
+        return min(start_with_0, n - start_with_0);
+    }
+};
+
+
