fa56ca98ce1bf8abf1e2e32b148759bef2787e54
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-02 22:53:52 +0530

    Add frequency counting approach for string equality check
    Implemented a new approach using an array as a hashmap to store character frequencies and check if characters can be redistributed evenly across strings.

--- DAY 16 - 1897/Redistribute Characters to Make All Strings Equal.cpp
+++ DAY 16 - 1897/Redistribute Characters to Make All Strings Equal.cpp
@@ -22,3 +22,33 @@ public:
         return true;
     }
 };
+
+//Approach-2 (Using array as hashmap for storing frequency)
+//T.C : O(n)
+//S.C : O(26)
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        int n = words.size();
+        
+        int count[26] = {0};
+        
+        for(string &word : words) {
+            
+            for(char &ch : word) {
+                count[ch-'a']++;
+            }
+            
+        }
+        
+        for(int i = 0; i < 26; i++) {
+            if(count[i]%n != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
+
