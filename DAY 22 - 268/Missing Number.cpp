1f5475ddbeae9518aacea5874bd6300a644c2dca
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-07 23:17:46 +0530

    Implement missing number solution using math
    Added a second approach using mathematical formula to find the missing number.

--- DAY 22 - 268/Missing Number.cpp
+++ DAY 22 - 268/Missing Number.cpp
@@ -12,3 +12,18 @@ public:
         return sum;
     }
 };
+
+//Approach-2 (Math)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        for(int i = 0; i<n; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
+};
