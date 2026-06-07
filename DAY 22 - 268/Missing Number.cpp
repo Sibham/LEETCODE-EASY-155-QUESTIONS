3ab7869307ab6507f21b8347f8ff60067949c120
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-07 23:18:27 +0530

    Add binary search method for missing number
    Implemented binary search approach to find the missing number in the array.

--- DAY 22 - 268/Missing Number.cpp
+++ DAY 22 - 268/Missing Number.cpp
@@ -27,3 +27,30 @@ public:
         return sum;
     }
 };
+
+
+//Approach-3 (Binary Search)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int l   = 0;
+        int r   = n-1;
+        int result = n;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] > mid) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        
+        return result;        
+    }
+};
