9dd4986f22ed60aebf7ca41258560551c04a3092
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-11 18:40:12 +0530

    Update Squares of a Sorted Array.cpp

--- DAY 24 - 977 /Squares of a Sorted Array.cpp
+++ DAY 24 - 977 /Squares of a Sorted Array.cpp
@@ -16,3 +16,32 @@ public:
     }
 };
 
+//Approach-2 (Follow Up, without sorting nlogn)
+//T.C : O(n)
+//S.C : O(1) - (Not considering the space takent to store the result)
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> result(n);
+        
+        int i = 0, j = n-1;
+        int k = n-1;
+        
+        while(k >= 0) {
+            int a = nums[i]*nums[i];
+            int b = nums[j]*nums[j];
+            
+            if(a > b) {
+                result[k] = a;
+                i++;
+            } else {
+                result[k] = b;
+                j--;
+            }
+            k--;
+        }
+        return result;
+    }
+};
