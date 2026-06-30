ffa366719412edc27559964b883ac80482a6ef15
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-30 18:55:52 +0530

    Create  MINIMUN COMMON VALUE.cpp

---
+++ DAY 26 - 2540 /MINIMUN COMMON VALUE.cpp
@@ -0,0 +1,18 @@
+//Approach-1 (Using set)
+//T.C : O(m+n)
+//S.C : O(m)
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st(begin(nums1), end(nums1));
+        
+        for(int &num : nums2) {
+            if(st.find(num) != st.end()) {
+                return num;
+            }
+        }
+        
+        return -1;
+    }
+};
+
