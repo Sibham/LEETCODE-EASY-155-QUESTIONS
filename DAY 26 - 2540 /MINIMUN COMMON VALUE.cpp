16302425fcbb63154232a423dd7ea72b7e70e4cb
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-30 18:56:47 +0530

    Add two-pointer solution for common value search
    Implemented a two-pointer approach to find the minimum common value between two arrays.

--- DAY 26 - 2540 /MINIMUN COMMON VALUE.cpp
+++ DAY 26 - 2540 /MINIMUN COMMON VALUE.cpp
@@ -49,3 +49,29 @@ public:
         return -1;
     }
 };
+
+//Approach-3 (Using 2 Pointer)
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        int i = 0; //for nums1
+        int j = 0; //for nums2
+        
+        while(i < m && j < n) {
+            if(nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return -1;
+    }
+};
