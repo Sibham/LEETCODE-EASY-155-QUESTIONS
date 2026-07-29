3b207d5ac5a5a023318026de1013dc2c350df6cf
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-29 14:53:45 +0530

    Create  Intersection of Two Arrays.cpp

---
+++ DAY 27 - 349 /Intersection of Two Arrays.cpp
@@ -0,0 +1,25 @@
+//Approach-1 (Using two sets)
+//T.C : O(m+n)
+//S.C : O(m+n)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st1(begin(nums1), end(nums1));
+        
+        unordered_set<int> st2;
+        
+        
+        for(int &num : nums2) {
+            if(st1.find(num) != st1.end()) {
+                st2.insert(num);
+            }    
+        }
+        
+        vector<int> result;
+        for(auto &it : st2) {
+            result.push_back(it);
+        }
+        
+        return result;
+    }
+};
