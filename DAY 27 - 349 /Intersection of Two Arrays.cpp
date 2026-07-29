f3cfb812e60b55b2b15d71af16dea565d859c299
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-29 14:54:11 +0530

    Update Intersection of Two Arrays.cpp

--- DAY 27 - 349 /Intersection of Two Arrays.cpp
+++ DAY 27 - 349 /Intersection of Two Arrays.cpp
@@ -23,3 +23,26 @@ public:
         return result;
     }
 };
+
+//Approach-2 (Using one set)
+//T.C : O(m+n)
+//S.C : O(m+n)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st(begin(nums1), end(nums1));
+        
+        vector<int> result;
+        
+        for(int &num : nums2) {
+            if(st.find(num) != st.end()) {
+                result.push_back(num);
+                st.erase(num);
+            }    
+        }
+        
+        return result;
+    }
+};
+
+
