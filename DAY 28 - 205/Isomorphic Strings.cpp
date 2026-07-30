1d02ab8758e92aef3cb15f5ef7a5e9d295ea3526
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-30 14:49:42 +0530

    Update Isomorphic Strings.cpp

--- DAY 28 - 205/Isomorphic Strings.cpp
+++ DAY 28 - 205/Isomorphic Strings.cpp
@@ -1,3 +1,6 @@
+//Using array of size 256
+//T.C : O(n)
+//S.C : O(1) -> Take array of size 256 to handle all ASCII characters instead of taking map
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
