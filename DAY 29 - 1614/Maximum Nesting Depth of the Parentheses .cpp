0def56084217d20078ddeb269204ee0952752f6e
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-31 16:05:33 +0530

    Update Maximum Nesting Depth of the Parentheses .cpp

--- DAY 29 - 1614/Maximum Nesting Depth of the Parentheses .cpp
+++ DAY 29 - 1614/Maximum Nesting Depth of the Parentheses .cpp
@@ -23,3 +23,27 @@ public:
     }
 };
 
+//Approach-2 (using constant space)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int maxDepth(string s) {
+        int result = 0;
+        int openBrackets = 0;
+
+        for (char &ch : s) {
+            if (ch == '(') {
+                openBrackets++;
+            } else if (ch == ')') {
+                openBrackets--;
+            }
+            
+            result = max(result, openBrackets);
+        }
+        
+        return result;
+    }
+};
+
+
