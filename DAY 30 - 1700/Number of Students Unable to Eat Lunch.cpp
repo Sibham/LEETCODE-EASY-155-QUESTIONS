a0a49e26045878030a5635028a9e19214f154cad
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-01 12:35:45 +0530

    Add second approach for counting students unable to eat
    Implemented a second approach using a counter to count students who cannot eat lunch based on their preferences.

--- DAY 30 - 1700/Number of Students Unable to Eat Lunch.cpp
+++ DAY 30 - 1700/Number of Students Unable to Eat Lunch.cpp
@@ -29,3 +29,26 @@ public:
         return que.size();
     }
 };
+
+//Aproach-2 (Using counter only)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int countStudents(vector<int>& stud, vector<int>& sand) {
+        int n = stud.size();
+        int arr[2] = {0};
+        //arr[0] = count of student who like circular
+        //arr[1] = count of student who like square
+          
+        for(int &x : stud)
+            arr[x]++;
+        
+        for(int i = 0; i<n; i++) {
+            if(arr[sand[i]] == 0) return n-i;
+            arr[sand[i]]--;
+        }
+        
+        return 0;
+    }
+};
