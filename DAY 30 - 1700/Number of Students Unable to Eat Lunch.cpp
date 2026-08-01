7f25b4df7edec8520d5bce2e8258c7b33bd44864
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-01 12:35:06 +0530

    Create Number of Students Unable to Eat Lunch.cpp

---
+++ DAY 30 - 1700/Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,31 @@
+//Approach-1 (Simply simulate whatever the problem asks for)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int len = students.size();
+        queue<int> que;
+        stack<int> st;
+
+        for (int i = 0; i < len; i++) {
+            st.push(sandwiches[len - i - 1]);
+            que.push(students[i]);
+        }
+
+        int lastServed = 0;
+        while (!que.empty() && lastServed < que.size()) {
+            if (st.top() == que.front()) {
+                st.pop();
+                que.pop();
+                lastServed = 0;
+            } else {
+                que.push(que.front());
+                que.pop();
+                lastServed++;
+            }
+        }
+
+        return que.size();
+    }
+};
