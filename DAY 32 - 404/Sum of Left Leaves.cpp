7e1ed1bd890858b571eba7926a625feff6bae091
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-03 15:28:05 +0530

    Create Sum of Left Leaves.cpp

---
+++ DAY 32 - 404/Sum of Left Leaves.cpp
@@ -0,0 +1,27 @@
+//Approach-1 (Using Parent Pointer)
+//T.C : O(n)
+//S.C : O(1) Auxiiliary Space and O(depth of tree) recursion system stack space
+class Solution {
+public:
+    int sum(TreeNode* curr, TreeNode* parent) {
+        if(!curr)
+            return 0;
+        
+        int left  = sum(curr->left, curr);
+        int right = sum(curr->right, curr);
+        
+        int sum = 0;
+        if(curr->left == NULL && curr->right == NULL) {
+            if(parent && parent->left == curr)
+                sum += curr->val;
+        }
+        
+        return left+right+sum;
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(!root)
+            return 0;
+        TreeNode* parent = NULL;
+        return sum(root, parent);
+    }
+};
