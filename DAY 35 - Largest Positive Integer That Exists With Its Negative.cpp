307a2fbd4375e7316fcdacdcf4eed901aafc8ada
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-06 22:44:51 +0530

    Update DAY 35 - Largest Positive Integer That Exists With Its Negative.cpp

--- DAY 35 - Largest Positive Integer That Exists With Its Negative.cpp
+++ DAY 35 - Largest Positive Integer That Exists With Its Negative.cpp
@@ -19,3 +19,29 @@ public:
         return result;
     }
 };
+
+//Approach-2 (Using Sorting + 2-Pointers)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        int i = 0;
+        int j = nums.size() - 1;
+        
+        while (i < j) {
+            if (-nums[i] == nums[j]) {
+                return nums[j];
+            } 
+            else if (-nums[i] < nums[j]) { 
+                j--;
+            }
+            else { 
+                i++;
+            }
+        }
+        return -1;
+    }
+};
