81d147174837c843eecb4edc4a9c16c1cfb937fb
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-08 17:57:38 +0530

    Update Relative Ranks.cpp

--- DAY 36 - 506/Relative Ranks.cpp
+++ DAY 36 - 506/Relative Ranks.cpp
@@ -77,6 +77,51 @@ public:
         }
         
         return result;
+    }
+};
+
+//Approach-3 (Using max heap)
+//T.C : O(n)
+//S.C : O(max_score)
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<string> result(n);
+        
+        int M = *max_element(begin(score), end(score));
+        
+        vector<int> mp(M+1, -1);
+        
+        for(int i = 0; i < n; i++) {
+            mp[score[i]] = i;
+        }
+        
+        
+        int rank = 1;
+        
+        for(int s = M; s >= 0; s--) {
+            
+            if(mp[s] != -1) {
+                int athlete = mp[s];
+                
+                if(rank == 1) {
+                    result[athlete] = "Gold Medal";
+                } else if(rank == 2) {
+                    result[athlete] = "Silver Medal";
+                } else if(rank == 3) {
+                    result[athlete] = "Bronze Medal";
+                } else {
+                    result[athlete] = to_string(rank);
+                }
+                
+                rank++;
+            }
+            
+        }
+        
+        return result;
         
         
     }
+};
