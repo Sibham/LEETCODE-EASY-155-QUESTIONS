d43ccedd70afe23d49bc2cb5071b3abb6525ca2d
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-12 12:17:22 +0530

    Implement optimal backtracking for subset XOR sum
    Added an optimal backtracking approach for calculating the sum of all subset XOR totals.

--- DAY 39 - 1863/Sum of All Subset XOR Totals.cpp
+++ DAY 39 - 1863/Sum of All Subset XOR Totals.cpp
@@ -34,3 +34,25 @@ public:
         return result;
     }
 };
+
+//Approach-2 (Using optimal backtracking)
+//T.C : O(2^n)
+//S.C : O(n)
+class Solution {
+public:
+
+    int solve(int i, vector<int>& nums, int Xor) {
+        if(i == nums.size()) {
+            return Xor;
+        }
+
+        int include = solve(i+1, nums, nums[i] ^ Xor);
+        int exclude = solve(i+1, nums, Xor);
+
+        return include + exclude;
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        return solve(0, nums, 0);
+    }
+};
