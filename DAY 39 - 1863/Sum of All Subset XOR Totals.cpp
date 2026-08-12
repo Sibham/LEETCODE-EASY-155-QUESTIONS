01946c4b51c9417ece693cabc9b7c92cfa837077
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-12 12:17:43 +0530

    Update Sum of All Subset XOR Totals.cpp

--- DAY 39 - 1863/Sum of All Subset XOR Totals.cpp
+++ DAY 39 - 1863/Sum of All Subset XOR Totals.cpp
@@ -56,3 +56,21 @@ public:
         return solve(0, nums, 0);
     }
 };
+
+//Approach-3 (Observing pattern)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+
+        for(int &num : nums) {
+            result |= num;
+        }
+
+        return result << (n-1);
+    }
+};
+
