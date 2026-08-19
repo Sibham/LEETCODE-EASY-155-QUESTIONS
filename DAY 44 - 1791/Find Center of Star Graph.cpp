89161bfce8e938d0e33bc76d880993b9fdd3fb74
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-08-19 18:34:34 +0530

    Update and rename Minimum Number of Moves to Seat Everyone.cpp to Find Center of Star Graph.cpp

---
+++ DAY 44 - 1791/Find Center of Star Graph.cpp
@@ -0,0 +1,29 @@
+//Approach-1 (Using map)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size();
+        unordered_map<int, int> degree;
+
+        for(auto &vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+
+            degree[u]++;
+            degree[v]++;
+        }
+
+        for(auto &it : degree) {
+            int node = it.first;
+            int deg  = it.second;
+
+            if(deg == n)
+                return node;
+        }
+
+        return -1;
+    }
+};
+
